910ecd703414bc1b23d85fb52d8185979b972b10
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-08-03 14:55:29 +0200

    Changes managed/discarded

---
+++ src/main/java/com/mycompany/mavenproject1/Classe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author pc-asus
+ */
+public class Classe  {
+     
+    class Book { 
+     // Book properties
+        String title; 
+      
+        String author; 
+        
+        int numberofpages;
+        
+        String publisher;
+    }           
+                           } 
