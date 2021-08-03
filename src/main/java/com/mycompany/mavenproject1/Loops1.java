1c9022e1767cf8120b5606f74f0f0eb4a7ddbc40
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-08-03 10:02:20 +0200

    Loops example

---
+++ src/main/java/com/mycompany/mavenproject1/Loops1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author pc-asus
+ */
+public class Loops1 {
+
+    public static void main(String[] args) {
+        for (int time = 0; time < 6; time++) {
+            System.out.println("On en a gros! " + time);
+        } 
+        while (true) {            
+            System.out.println("ZUUUUT");
+        }
+    }
+}
