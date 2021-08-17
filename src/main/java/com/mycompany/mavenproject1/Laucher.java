273c62a08948d78281be11545defe7df502d95c1
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-08-17 11:16:12 +0200

    Lambda Testing

---
+++ src/main/java/com/mycompany/mavenproject1/Laucher.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author pc-asus
+ */
+public class Laucher {
+    
+  static int  operate(int operand11, operand22, InterfaceFonctionel1 myInterface ) {
+    return myInterface.operation(operand11, operand22);
+}
+  public static main(string arg[]){
+      
+  }
+}
