2bf60fa13ee7bf8d89ad4d2de81e3044af539ccb
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-08-23 10:03:20 +0200

    variables

---
+++ src/main/java/com/mycompany/mavenproject1/NewClass2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author pc-asus
+ */
+public class NewClass2 {
+    double nbre1 = 10, nbre2 = 3;
+int resultat = (int)(nbre1 / nbre2)   
+System.out.printIn("Le resultat est = " + resultat);
+
+        
+}
