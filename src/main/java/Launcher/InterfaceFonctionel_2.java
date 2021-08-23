4421caffc2a51e6a91a50354680683a73b7f9d95
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-08-23 11:04:17 +0200

    Organizing

---
+++ src/main/java/Launcher/InterfaceFonctionel_2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Launcher;
+
+/**
+ *
+ * @author pc-asus
+ */
+public interface InterfaceFonctionel_2 {
+    void SayMessage(sting message);
+
+    public void SayMessage(String good_Game);
+    
+}
