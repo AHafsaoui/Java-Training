adb15da26c195612d8aa76cbbabbd6e46c8af0e0
AHafsaoui <88088032+AHafsaoui@users.noreply.github.com>
2021-08-24 09:45:29 +0200

    Loops modification

---
+++ src/main/java/com/mycompany/mavenproject1/Loops.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author pc-asus
+ */
+public class Loops {
+   
+ 
+    }
+
+ 
+for (int V = 0; V<101;V++){
+
+}
